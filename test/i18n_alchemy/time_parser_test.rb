a23d791997d1a7cb54dfe5c13b353d460b355c8b
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2011-01-30 23:07:51 -0200

    Refactor Time/DateTime tests

--- test/i18n_alchemy/time_parser_test.rb
+++ test/i18n_alchemy/time_parser_test.rb
@@ -1,11 +1,6 @@
 require "test_helper"
 
-class TimeParserTest < MiniTest::Unit::TestCase
-  def setup
-    @parser = I18n::Alchemy::TimeParser
-    @time   = Time.new(2011, 12, 31, 12, 15, 45)
-  end
-
+module BaseTimeParserTest
   def test_does_not_convert_non_string_objects
     assert_equal @time, @parser.parse(@time)
   end
@@ -41,6 +36,15 @@ class TimeParserTest < MiniTest::Unit::TestCase
   end
 end
 
+class TimeParserTest < MiniTest::Unit::TestCase
+  def setup
+    @parser = I18n::Alchemy::TimeParser
+    @time   = Time.new(2011, 12, 31, 12, 15, 45)
+  end
+
+  include BaseTimeParserTest
+end
+
 
 class DateTimeParserTest < MiniTest::Unit::TestCase
   def setup
@@ -48,37 +52,5 @@ class DateTimeParserTest < MiniTest::Unit::TestCase
     @time   = DateTime.new(2011, 12, 31, 12, 15, 45)
   end
 
-  def test_does_not_convert_non_string_objects
-    assert_equal @time, @parser.parse(@time)
-  end
-
-  def test_parses_valid_string_times_with_default_i18n_locale
-    assert_equal "2011-12-31 12:15:45 -0200",
-      @parser.parse("12/31/2011 12:15:45")
-  end
-
-  def test_parsers_string_times_on_current_i18n_locale
-    I18n.with_locale :pt do
-      assert_equal "2011-12-31 12:15:45 -0200",
-        @parser.parse("31/12/2011 12:15:45")
-    end
-  end
-
-  def test_parsers_returns_the_given_string_when_invalid_time
-    assert_equal "31/12/2011 12:15:45", @parser.parse("31/12/2011 12:15:45")
-  end
-
-  def test_does_not_localize_string_values
-    assert_equal "12/31/2011 12:15:45", @parser.localize("12/31/2011 12:15:45")
-  end
-
-  def test_localizes_time_values
-    assert_equal "12/31/2011 12:15:45", @parser.localize(@time)
-  end
-
-  def test_localizes_time_values_based_on_current_i18n_locale
-    I18n.with_locale :pt do
-      assert_equal "31/12/2011 12:15:45", @parser.localize(@time)
-    end
-  end
+  include BaseTimeParserTest
 end
