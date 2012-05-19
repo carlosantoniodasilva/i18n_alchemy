d99837c8b065b874e7c6d5148eb14f669eea5af3
Cairo Noleto <caironoleto@gmail.com>
2012-05-19 17:35:42 -0300

    Adding support to use custom parsers.

---
+++ test/custom_parsers/my_custom_date_parser.rb
@@ -0,0 +1,14 @@
+module MyCustomDateParser
+  include I18n::Alchemy::DateParser
+  extend self
+
+  def localize(value)
+    I18n.localize value, :format => :custom
+  end
+
+  protected
+
+  def i18n_format
+    I18n.t(:custom, :scope => [:date, :formats])
+  end
+end
