6ee479b521648cd72b4dbb1676a0619b2142077f
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-11-04 22:23:31 -0200

    Fix input tests due to attributes order
    Rails decided to trade output consistency for speed, removing the
    attributes sorting when generating tags. This means now tags will be
    generated based on the order of the passed hash.
    
    https://github.com/rails/rails/commit/03d30ce71ca17cec82774a86d2acc87e2f153e53

--- test/action_view_test.rb
+++ test/action_view_test.rb
@@ -21,35 +21,43 @@ class ActionViewTest < I18n::Alchemy::TestCase
   end
 
   def test_text_field_with_string_attribute
-    assert_equal text_input(:name, 'Potato'),
-      @template.text_field(:product, :name, :object => @localized)
+    assert_same_text_input :name, 'Potato'
   end
 
   def test_text_field_with_integer_attribute
-    assert_equal text_input(:quantity, '10'),
-      @template.text_field(:product, :quantity, :object => @localized)
+    assert_same_text_input :quantity, '10'
   end
 
   def test_text_field_with_decimal_attribute
-    assert_equal text_input(:price, '1,99'),
-      @template.text_field(:product, :price, :object => @localized)
+    assert_same_text_input :price, '1,99'
   end
 
   def test_text_field_with_date_attribute
-    assert_equal text_input(:released_at, '28/02/2011'),
-      @template.text_field(:product, :released_at, :object => @localized)
+    assert_same_text_input :released_at, '28/02/2011'
   end
 
   def test_text_field_with_time_attribute
-    assert_equal text_input(:last_sale_at, '28/02/2011 13:25:30'),
-      @template.text_field(:product, :last_sale_at, :object => @localized)
+    assert_same_text_input :last_sale_at, '28/02/2011 13:25:30'
   end
 
   private
 
-  def text_input(attribute_name, value)
+  # Since we do not want to rely on Rails dom-assertions (neither from the
+  # framework nor from the extracted gem), we build our own :).
+  def assert_same_text_input(attribute, value)
+    assert_includes [
+      text_input_sorted_attributes(attribute, value),
+      text_input_unsorted_attributes(attribute, value)
+    ], @template.text_field(:product, attribute, :object => @localized)
+  end
+
+  def text_input_sorted_attributes(attribute_name, value)
     size = rails4? ? ' ' : ' size="30" '
     %Q[<input id="product_#{attribute_name}" name="product[#{attribute_name}]"#{size}type="text" value="#{value}" />]
+  end
+
+  def text_input_unsorted_attributes(attribute_name, value)
+    %Q[<input type="text" value="#{value}" name="product[#{attribute_name}]" id="product_#{attribute_name}" />]
   end
 
   def rails4?
